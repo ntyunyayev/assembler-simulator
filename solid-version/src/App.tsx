71f511997f70fa05b93cc33e46ffba5ee1f5bcd7
Pierre-Yves Haubruge <pierre-yves.haubruge@student.uclouvain.be>
2025-06-25 16:45:12 +0200

    screen module

--- solid-version/src/App.tsx
+++ solid-version/src/App.tsx
@@ -12,6 +12,7 @@ import Memory from './memory.tsx';
 import Settings from './settings.tsx';
 import Flags from './Flags.tsx';
 import Labels from './Labels.tsx';
+import Screen from './Screen.tsx';
 
 function App() {
     const [state, setState] = createStateStore();
@@ -20,46 +21,20 @@ function App() {
         <StateContext.Provider value={[state, setState]}>
             <Navbar/>
             <CodeActions/>
-            <div class="row main">
-                <Editor/>
-            <div class="col1">
-                <Labels/>  
-                <Memory/>
-            </div>
+            <div class="columns">
+                <div class="col0">
+                    <Editor/>
+                </div>
+                <div class="col1">
+                    <Labels/>  
+                    <Memory/>
+                </div>
                 <div class="col2">
-                    <Settings></Settings>
-
-                    <h4>Screen Display</h4>
-                    <div class="screen-container">
-                        <div tabindex="0"
-                            record-keys
-                            ng-class="{'screen': true, 'screen-recording': recordingKeys}"
-                            ng-mouseenter="recordingKeys = true; focusScreen()"
-                            ng-mouseleave="recordingKeys = false; focusScreen()"
-                        >
-                            <table class="screen-table">
-                                <tbody>
-                                    <tr ng-repeat="row in screenPixels track by $index">
-                                        <td ng-repeat="pixel in row track by $index">
-                                            <div
-                                                class="screen-pixel"
-                                                ng-class="{
-                'screen-pixel-on': pixel === 1,
-                'screen-pixel-off': pixel === 0,
-                'screen-pixel-ascii': pixel !== 1 && pixel !== 0
-                }"
-                                            ></div>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-        <Flags></Flags>
-
-
-                        </div>
-                    </div>
+                    <Settings/>
+                    <Screen/>
+                    <Flags></Flags>
+                </div>
+            </div>
         </StateContext.Provider>
     )
 }
