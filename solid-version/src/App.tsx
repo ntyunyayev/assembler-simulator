e52a6f980faf55f3bfcbd9c043bb66aa7e9b9cc5
Pierre-Yves Haubruge <pierre-yves.haubruge@student.uclouvain.be>
2025-06-25 14:22:58 +0200

    first correction in app.tsx

--- solid-version/src/App.tsx
+++ solid-version/src/App.tsx
@@ -98,22 +98,17 @@ function App() {
                 <div class="alert alert-danger" ng-hide="error === ''">ERROR</div> {/* TODO: Fix this */}
                 <div class="row main">
                     <div class="editor-container">
-                        <div class=" ">
-                            <div class="">
-                                <h4 class="">
+                        <div>
+                            <div>
+                                <h4>
                                     Code
-                                    <small
-                                    >(<a
-                                        href="./instruction-set.html"
-                                        target="_blank"
-                                        style="color: #337ab7"
-                                    >Instruction Set</a
-                                        >)</small
-                                    >
+                                    <small>(
+                                        <a class="instruction-set-button" href="./instruction-set.html" target="_blank">Instruction Set</a>
+                                    )</small>
                                 </h4>
                             </div>
                             <div>
-                                <div class="editor" id="editor" contenteditable="true" spellcheck="false"
+                                <div class="editor" id="editor"
                                     select-line
                                     ng-blur="updateCode($event)"
                                     ng-keyup="updateCode($event)">
@@ -131,9 +126,9 @@ function App() {
                         </div>
                     </div>
                     <div class="col1">
-                        <div class=" ">
-                            <div class="">
-                                <h4 class="">Labels</h4>
+                        <div>
+                            <div>
+                                <h4>Labels</h4>
                             </div>
                             <div class=" source-code">
                                 <table class="labels-table">
@@ -144,7 +139,7 @@ function App() {
                                             <th>Value</th>
                                         </tr>
                                         <tr ng-repeat="(name, value) in labels" class="codelabel">
-                                            <td class="codelabel-name">{{ name }}</td>
+                                            <td class="codelabel-name">{name}</td>
                                             {/*
                                 <td class="codelabel-line">
                                     <a ng-click="jumpToLine(value)"  ng-mouseenter="setHighlight(value)"   ng-mouseleave="setHighlight(-1)">
@@ -181,18 +176,20 @@ function App() {
                         </div>
                     </div>
                     <div class="col2">
-                        <div
-                            class="settings-icon-container"
-                            onclick="document.querySelectorAll('.settings-container').forEach(
-              (el) => {
-                if (el.style.display === 'block') {
-                  el.style.display = 'none';
-                } else {
-                  el.style.display = 'block';
-                }
-              }
-            )"
-                            style="cursor:pointer;">
+            <div
+                class="settings-icon-container"
+                onClick={() => {
+                  document.querySelectorAll('.settings-container').forEach(
+                    (el) => {
+                      if ((el as HTMLElement).style.display === 'block') {
+                        (el as HTMLElement).style.display = 'none';
+                      } else {
+                        (el as HTMLElement).style.display = 'block';
+                      }
+                    }
+                  );
+                }}
+                style="cursor:pointer;">
                             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-settings-icon lucide-settings"><path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" /><circle cx="12" cy="12" r="3" /></svg>
                         </div>
 
